3e1edcefa2685940c64e5768c9b1de17f2dbfca1
Rija Nadeem <rijanadeem42@gmail.com>
2020-06-17 02:25:20 +0500

    chap 26 34

--- code/app.js
+++ code/app.js
@@ -100,7 +100,7 @@
 //     document.write(input+" doesnot found");
 // }
 
-//task 15 ***************************************
+//task 15 ************************ not done *************************
 
 //task 16
 // var university = "University of Karachi";
@@ -124,5 +124,136 @@
 // }
 // document.write("Text: "+text+"<br>");
 // document.write("There are "+counter+" occurence(s) of the word 'the' ");
+
+
+// chapter 26-30
+
+//task 1
+// var num= 3.45214;
+// document.write("number: "+num+"<br>");
+// document.write("number: "+Math.round(num)+"<br>");
+// document.write("number: "+Math.floor(num)+"<br>");
+// document.write("number: "+Math.ceil(num)+"<br>");
+
+//task 2
+// var num= -2.673;
+// document.write("number: "+num+"<br>");
+// document.write("number: "+Math.round(num)+"<br>");
+// document.write("number: "+Math.floor(num)+"<br>");
+// document.write("number: "+Math.ceil(num)+"<br>");
+
+//task 3
+// var num= +prompt("Enter a number");
+// document.write("The absolute value of "+num+" is "+Math.abs(num));
+
+//task 4
+// document.write("random dice value: "+Math.ceil(Math.random() * 6));
+
+//task 5
+//var val= Math.ceil(Math.random() * 2);
+// if(val===1){
+//     document.write("random coin value: Tails ");
+// } else{
+//     document.write("random coin value: Heads ");
+// }
+
+//task 6
+//document.write("random number between 1 and 100: "+Math.ceil(Math.random() * 100));
+
+//task 7    *****************samjh ni aya question
+
+//task 8
+// var secret= Math.ceil(Math.random() * 10) ;
+// var input= +prompt("Enter a number between 1 and 10");
+// if(input===secret){
+//     alert("you won");
+// }
+// else{
+//     alert("try again");
+// }
+
+
+// chapter 31-34
+
+//task 1
+// var today= new Date();
+// document.write(today);
+
+//task 2
+// var today= new Date();
+// var months=["January", "February", "March", "April", "May", "June", "July","August","September","October","November","December"];
+// document.write("Current month: "+months[today.getMonth()]);
+
+//task 3
+// var today= new Date();
+// var days=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+// document.write("Today is: "+days[today.getDay()]);
+
+//task 4
+// var today= new Date();
+// var days=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+// if(today.getDate()==="0" || today.getDate()==="6" ){
+//     document.write("It's Fun day");
+// } else{
+//     document.write("Not sunday or saturday");
+// }
+
+//task 5
+// var today= new Date();
+// if(today.getDate()<16){
+//     document.write("First fifteen days of the month");
+// }else{
+//     document.write("Last days of the month");
+// }
+
+//task 6
+// var today= new Date();
+// var msec=today.getTime();
+// document.write("Current Date: "+today+"<br>");
+// document.write("Elapsed milliseconds since Jan 1, 1970: "+msec+"<br>");
+// document.write("Elapsed minutes since Jan 1, 1970: "+(msec/1000*60)+"<br>");
+
+//task 7 ******************check in morning*****************
+// var today= new Date();
+// if(today.getHours()<=12){
+//     alert("Its AM");
+// } else{
+//     alert("Its PM");
+// }
+
+//task 8
+// var laterDate= new Date("dec 31 2020 00:00:00");
+// document.write(laterDate);
+
+//task 9 ************** ans sahi ni ara**************
+// var ramadan= new Date("June 18 2015");
+// var today= new Date();
+// var ramadanTime= ramadan.getTime();
+// var todayTime= today.getTime();
+// var diff= todayTime-ramadanTime;
+// var days= Math.floor(diff/(1000*60*60*24));
+// alert(days+" days have passed since 1st Ramadan, 2015");
+
+//task 10
+// var refDate= new Date("Dec 5 2015 22:50:16");
+// var refDateTime= refDate.getTime();
+// var startDate= new Date("Jan 01 2015");
+// var startDateTime= startDate.getTime();
+// var diff= refDateTime - startDateTime;
+// var acc= Math.floor(diff/(1000*60));
+//document.write("On reference date "+refDate+" ,"+acc+" seconds had passed since beginning of 2015")
+
+// task 11 12 ********** not done *******************
+
+
+//task 13
+// var dob= new Date(prompt("Enter your birth day"));
+// var dobTime= dob.getTime();
+// var today= new Date();
+// var todayTime= today.getTime();
+// var diff= todayTime-dobTime;
+// var acc= Math.floor(diff/(1000*60*60*24*30*12));
+// document.write("Your age is "+acc+"<br>");
+// document.write("Your birth year is "+dob.getFullYear());
 
 
